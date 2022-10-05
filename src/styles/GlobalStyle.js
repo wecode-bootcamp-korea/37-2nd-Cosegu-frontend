e0a5cfdd8aba18113e8f3135e2c0f8e8aa140e16
rubi <rubi0624@nate.com>
2022-10-05 14:55:19 +0900

    global, theme 수정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -9,38 +9,6 @@ const GlobalStyle = createGlobalStyle`
         font-family: ‘Noto Sans KR’, sans-serif;
         line-height: 1.3em;
     }
-
-    .cotain {
-        width: 1200px;
-        margin: 0 auto;
-    }
-
-    .hide {
-        position: absolute;
-        overflow: hidden;
-        width: 0;
-        height: 0;
-        margin: 0;
-        padding: 0;
-        font-size: 0;
-        line-height: 0;
-        text-indent: -999em;
-      }
-
-      input{
-        border: 1px solid #ddd; 
-        
-        &:foucus{
-            outline: none;
-        }
-      }
-
-      button{
-        border: none;
-        cursor: pointer;
-        background: black;
-        color: white;
-      }
 `;
 
 export default GlobalStyle;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,30 @@
 const theme = {
   mainColor: "#BF1010",
+  contain: {
+    width: "1200px",
+    margin: "0 auto",
+  },
+  hide: {
+    position: "absolute",
+    overflow: "hidden",
+    width: "0",
+    height: "0",
+    margin: "0",
+    padding: "0",
+    fontSize: "0",
+    lineHeight: "0",
+    textIndent: "-999em",
+  },
+  input: {
+    border: "1px solid #ddd",
+    outline: "none",
+  },
+  button: {
+    border: "none",
+    background: "#000",
+    color: "#fff",
+    cursor: "pointer",
+  },
 };
 
 export default theme;
