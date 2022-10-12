6e247b6071db82a678af5876bab82a068d9fdfed
Shin Ju An <shinjuan0101@gmail.com>
2022-10-12 15:11:24 +0900

    Add : config 파일 추가

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const BASE_URL = "http://172.20.10.3:3000";
+
+export const API = {
+  LOGIN: `${BASE_URL}`,
+  RECRUITMENT: `${BASE_URL}`,
+  FAQ: `${BASE_URL}`,
+  CREWRECRUIT: `${BASE_URL}`,
+  MAIN: `${BASE_URL}`,
+  MYPAGE: `${BASE_URL}`,
+  WISHLIST: `${BASE_URL}`,
+  HEADER: `${BASE_URL}`,
+};
